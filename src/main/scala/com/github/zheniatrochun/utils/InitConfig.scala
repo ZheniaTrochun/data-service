02f1a72afcd6495a8d566213bd2a599027b564f8
Zhenia Trochun <zheniatrochun@gmail.com>
2018-02-21 14:57:27 +0200

    bug fixed

--- src/main/scala/com/github/zheniatrochun/utils/InitConfig.scala
+++ src/main/scala/com/github/zheniatrochun/utils/InitConfig.scala
@@ -7,25 +7,26 @@ import com.redis.RedisClient
 object InitConfig extends Config {
   private val redisClient = new RedisClient(new URI(config.getString("redis.url")))
 
+
   def cacheInitialConfigs(): Map[String, String] = {
-    if (!redisClient.connected)
-      redisClient.connect
+//    if (!redisClient.connected)
+//      redisClient.connect
 
     val res = redisClient.hmget[String, String]("data-service-config", "*")
       .getOrElse(Map.empty[String, String])
 
-    redisClient.disconnect
+//    redisClient.disconnect
 
     res
   }
 
   def createDummyConfig(): Unit = {
-    if (!redisClient.connected)
-      redisClient.connect
+//    if (!redisClient.connected)
+//      redisClient.connect
 
     val conf = Map("Sertificate" -> "123")
     redisClient.hmset("data-service-config", conf)
 
-    redisClient.disconnect
+//    redisClient.disconnect
   }
 }
