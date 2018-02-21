5f8b8b6010ff49fe8a1c75023abb0b785376215c
Zhenia Trochun <zheniatrochun@gmail.com>
2018-02-21 15:39:54 +0200

    bug fixed

--- src/main/scala/com/github/zheniatrochun/utils/InitConfig.scala
+++ src/main/scala/com/github/zheniatrochun/utils/InitConfig.scala
@@ -3,10 +3,12 @@ package com.github.zheniatrochun.utils
 import java.net.URI
 
 import com.redis.RedisClient
+import org.slf4j.LoggerFactory
 
 object InitConfig extends Config {
   private val redisClient = new RedisClient(new URI(config.getString("redis.url")))
 
+  private val logger = LoggerFactory.getLogger(this.getClass)
 
   def cacheInitialConfigs(): Map[String, String] = {
 //    if (!redisClient.connected)
@@ -14,6 +16,8 @@ object InitConfig extends Config {
 
     val res = redisClient.hmget[String, String]("data-service-config", "*")
       .getOrElse(Map.empty[String, String])
+
+    res.foreach(p => logger.info(s"TEST CONFIG MAP - $p"))
 
 //    redisClient.disconnect
 
