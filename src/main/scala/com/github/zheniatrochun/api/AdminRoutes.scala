6850ce3bdfacfbfcdc35b54251cef8b438e789ed
Zhenia Trochun <zheniatrochun@gmail.com>
2018-02-22 12:14:44 +0200

    fixed admin routes for config

--- src/main/scala/com/github/zheniatrochun/api/AdminRoutes.scala
+++ src/main/scala/com/github/zheniatrochun/api/AdminRoutes.scala
@@ -2,17 +2,23 @@ package com.github.zheniatrochun.api
 
 import akka.http.scaladsl.server.Directives._
 import com.github.zheniatrochun.services.AdminService
-import com.github.zheniatrochun.utils.{InitConfig, RouteUtils}
+import com.github.zheniatrochun.utils.{AppConfig, InitConfig, RouteUtils}
 
 
 class AdminRoutes(val adminService: AdminService)
-  extends RouteUtils {
+  extends RouteUtils with AppConfig {
 
   val routes = {
     pathPrefix("admin") {
       pathPrefix("config") {
-        (path("create-config") & get) {
+        (path("create-dummy") & get) {
           InitConfig.createDummyConfig()
+          config.update()
+          complete("Ok")
+        } ~
+        (path("update") & get) {
+          InitConfig.createDummyConfig()
+          config.update()
           complete("Ok")
         }
       } ~
